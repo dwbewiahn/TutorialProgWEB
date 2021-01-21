488d14506b797c32e817cce5b49704a85745bc72
Douglas Bewiahn <dwbewiahn@gmail.com>
2021-01-21 21:17:45 +0000

    Lab Guide for Javascript
    Arrays, objects, loops and functions

--- CODE/public/javascripts/grading.js
+++ CODE/public/javascripts/grading.js
@@ -1,3 +1,40 @@
+
+function resetAll() {
+
+    document.getElementById("gradeStudent").reset();
+    document.getElementById("finalGrade").innerHTML = "";
+
+}
+
+var students = [
+    { name: "Mary", id: 101 },
+    { name: "John", id: 202 },
+    { name: "Anthony", id: 303 }
+];
+
+var units = [
+    { name: "Mathematics", semester: "1º Semester", etcs: 6 },
+    { name: "physics", semester: "2º Semester", etcs: 6 },
+    { name: "Literature", semester: "3º Semester", etcs: 3 },
+];
+
+window.onload = function () {
+
+    let studentsOpt = "<option value=''></option>";
+    for (let i in students) {
+        studentsOpt += "<option value='" + i + "'>" + students[i].name + "</option>";
+    }
+    document.getElementById("studentName").innerHTML = studentsOpt;
+
+
+    let unitsOpt = "<option value=''></option>";;
+    for (let i in units) {
+        unitsOpt += "<option value='" + i + "'>" + units[i].name + "</option>"
+    }
+    document.getElementById("unitName").innerHTML = unitsOpt;
+}
+
+
 function getCalculate() {
 
     let studentName = document.getElementById("studentName").value;
@@ -9,10 +46,10 @@ function getCalculate() {
     let testGrade = parseFloat(document.getElementById("testGrade").value);
     let perTestGrade = parseFloat(document.getElementById("testGradePerc").value);
 
-   
+
     let finalScore = "";
 
-    if (studentName == "" || unitName == "" || isNaN(proGrade) || isNaN(perProGrade)  || isNaN(testGrade)  || isNaN(perTestGrade)) {
+    if (studentName == "" || unitName == "" || isNaN(proGrade) || isNaN(perProGrade) || isNaN(testGrade) || isNaN(perTestGrade)) {
         alert("Fill all the required fields")
         return;
     }
@@ -26,13 +63,10 @@ function getCalculate() {
 
     }
 
-    document.getElementById("finalGrade").innerHTML = "Student " + studentName + " obtained " + finalScore.toFixed(1) + " on the " + unitName + " unit.";
+    let studentIndex = document.getElementById("studentName").value;
+    let unitIndex = document.getElementById("unitName").value;
+
+    document.getElementById("finalGrade").innerHTML = "Student " + students[studentIndex].name + " with number "+students[studentIndex].id+" obtained " + finalScore.toFixed(1) + " on the " + units[unitIndex].name + " unit ("+units[unitIndex].etcs +" ETCS) of the "+units[unitIndex].semester+"";
 
 }
 
-function resetAll(){
-
-    document.getElementById("gradeStudent").reset();
-    document.getElementById("finalGrade").innerHTML = "";
-
-}
--- CODE/public/javascripts/studentGrades.js
+++ CODE/public/javascripts/studentGrades.js
@@ -1,13 +1,12 @@
-
-
+/* ******Exercicio passado****
 var name = "JohnSmith";
 var unit1 = "Mathematics";
 var gradeUnit1 = 7.5;
 var unit2 = "Literature";
-var gradeUnit2 = 11.2;
+var gradeUnit2 = 11.2; */
 
+/* ******Exercicio passado****
 window.onload = function () {
- 
     document.getElementById("student").innerHTML = name+" Grades"
 
     let gradeGrid = "";
@@ -25,5 +24,54 @@ window.onload = function () {
          gradeGrid += "<section><div class='twoFirstLetters'><h3>"+unit2.substring(0,2)+"</h3></div><div><h1>"+unit2+"</h1>"+"<p>Grade: "+gradeUnit2+"</p></div></section>";
     }
     document.getElementById("grades").innerHTML = gradeGrid;
+} */
+
+var name = "JohnSmith";
+var student = [
+    { unit: "Mathematics", grade: 8.3, semester: "3º semester", etcs: 6 },
+    { unit: "Literature", grade: 11.2, semester: "2º semester", etcs: 6 },
+    { unit: "Laws", grade: 18.5, semester: "1º semester", etcs: 3 },
+    { unit: "Informatics", grade: 14.3, semester: "1º semester", etcs: 6 },
+    { unit: "Cooking", grade: 7.4, semester: "2º semester", etcs: 3 }
+];
+
+function createUnit(unitInfo) {
+
+    let status = "";
+    if (unitInfo.grade < 9.5) {
+        status = "class='fail'";
+    }
+    return "<section " + status + " ><div class='twoFirstLetters'><h3>" + unitInfo.unit.substring(0, 2) + "</h3></div><div><h1>" + unitInfo.unit + "</h1>" + "<p>Grade: " + unitInfo.grade + "</p><p>Semester: " + unitInfo.semester + "</p><p>ETCS: " + unitInfo.etcs + "</p></div></section>";
+}
+
+window.onload = function () {
+    document.getElementById("student").innerHTML = name + " Grades"
+
+    let gradeGrid = "";
+    for (let i in student) {
+        gradeGrid += createUnit(student[i]);
+    }
+
+    document.getElementById("grades").innerHTML = gradeGrid;
+
+    let gradesSum = 0;
+    let unitsPassed = 0;
+    let unitsFail = 0;
+    let etcsSUm = 0;
+
+    for (let i in student) {
+        if (student[i].grade < 9.5) {
+            unitsFail++;
+        }
+        else {
+            gradesSum += (student[i].grade) * (student[i].etcs);
+            etcsSUm += student[i].etcs
+            unitsPassed++;
+        }
+
+    }
+    document.getElementById("summary").innerHTML = "<summary>Avarage: " + (gradesSum / etcsSUm).toFixed(1) + "</summary><p>Failed Units: " + unitsFail + "</p><p>Passed Units: " + unitsPassed + "</p>"
+
 
 }
+
